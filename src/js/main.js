49e67b3491109e80337c1b343488a91ae1c7cf69
Alex Balay <alexbalay94@gmail.com>
2023-02-19 15:57:35 -0300

    ff

--- src/js/main.js
+++ src/js/main.js
@@ -135,3 +135,9 @@ buscarPorDocumento.addEventListener("click", ()=>{
       }
     })
   })
+  Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'error',
+    confirmButtonText: 'Cool'
+  })
